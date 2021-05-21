32e05c37d39581e5992f1873803c24530176fe18
Anuj Subedi <struggleage@gmail.com>
2021-05-21 14:27:33 +0545

    👻 Initial commit
    👻 Initial commit

---
+++ includes/class-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://addonify.com/
+ * @since      1.0.0
+ *
+ * @package    Addonify_Gutenberg_Block_Image_Gallery_Lightbox
+ * @subpackage Addonify_Gutenberg_Block_Image_Gallery_Lightbox/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Addonify_Gutenberg_Block_Image_Gallery_Lightbox
+ * @subpackage Addonify_Gutenberg_Block_Image_Gallery_Lightbox/includes
+ * @author     Addonify <addonify@gmail.com>
+ */
+class Addonify_Gutenberg_Block_Image_Gallery_Lightbox_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
